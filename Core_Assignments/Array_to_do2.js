2326e5e5d3be2f3ee75fb6fcf3820c8b73e8a358
Evan Pebbles <78051545+Epebbles@users.noreply.github.com>
2021-09-06 22:24:39 -0700

    arrays2

--- Core_Assignments/Array_to_do2.js
+++ Core_Assignments/Array_to_do2.js
@@ -23,26 +23,65 @@
 // Third: minimize memory usage. With no new array, handle arrays/shiftBys in the millions.
 // Fourth: minimize the touches of each element.
 
-function rotateArr(arr, shiftBy) {
-    if (shiftBy > 0) {
-        for( var i = 0; i < shiftBy; i++) {
-            var temp = arr[arr.length - 1];
-            for (var z = arr.length -2; z >= 0; z--) {
-                arr[z+1] = arr[z];
-            }
-            arr[0] = temp;
-        }
-    } else {
-        var rotateLeft = Math.abs(shiftBy);
-        for( var i = 0; i < rotateLeft; i++) {
-            var temp = arr[0];
-            for (var z = 1; z < arr.length; z++) {
-                arr[z-1] = arr[z];
-            }
-            arr[arr.length - 1] = temp;
-        }
+// function rotateArr(arr, shiftBy) {
+//     if (shiftBy > 0) {
+//         for( var i = 0; i < shiftBy; i++) {
+//             var temp = arr[arr.length - 1];
+//             for (var z = arr.length -2; z >= 0; z--) {
+//                 arr[z+1] = arr[z];
+//             }
+//             arr[0] = temp;
+//         }
+//     } else {
+//         var rotateLeft = Math.abs(shiftBy);
+//         for( var i = 0; i < rotateLeft; i++) {
+//             var temp = arr[0];
+//             for (var z = 1; z < arr.length; z++) {
+//                 arr[z-1] = arr[z];
+//             }
+//             arr[arr.length - 1] = temp;
+//         }
+//     }
+// }
+// var myArr = [1,2,3];
+// rotateArr(myArr, -1);
+// console.log(myArr);
+
+//Filter Range
+//Alan is good at breaking secret codes. One method is to eliminate values that lie outside of a specific known range. Given arr and values min and max, retain only the array values between min and max. Work in-place: return the array you are given, with values in original order. No built-in array functions.
+
+// function filterRange(arr, minValue, maxValue) {
+//     for (var i = 0; i < arr.length; i++) {
+//         if (arr[i] < minValue || arr[i] > maxValue) {
+//             for  (var z = i+1; z < arr.length; z++) {
+//                 arr[z-1] = arr[z];
+//             }
+//             arr.length--;
+//             i--;
+//         }
+//     }
+// }
+// var myArr = [5,8,4,2,1,3];
+// filterRange(myArr, 3, 6);
+// console.log(myArr);
+
+
+//Concat
+//Replicate JavaScript's concat(). Create a standalone function that accepts two arrays. Return a new array containing the first array's elements, followed by the second array's elements. Do not alter the original arrays. Ex.: arrConcat( ['a','b'], [1,2] ) should return new array ['a','b',1,2].
+
+function concatArrays(arr1, arr2) {
+    var newArr = [];
+    var currentIdx = 0;
+    for (var i = 0; i < arr1.length; i++) {
+        newArr[currentIdx] = arr1[i];
+        currentIdx++;
     }
+    for (var i = 0; i < arr2.length; i++) {
+        newArr[currentIdx] = arr2[i];
+        currentIdx++;
+    }
+    return newArr;
 }
-var myArr = [1,2,3];
-rotateArr(myArr, -1);
-console.log(myArr);
+
+var results = concatArrays([1, 3, 5], ['a', 'b'])
+console.log(results);
